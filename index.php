7f0b6041151761cc18a8183f2eb182df29efe138
Gustavo Miguel <gustavo.biscaro@gmail.com>
2021-03-31 15:59:13 -0300

    connection with database and implements of php

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
-﻿<fieldset>
+﻿<?php
+try {
+    $pdo = new PDO("mysql:dbname=projeto_comentarios;host=127.0.0.1", "root", "");
+} catch (PDOException $e) {
+    echo "ERRO: ".$e->getMessage();
+    exit;
+}
+
+if(isset($_POST['nome']) && !empty($_POST['nome'])){
+    $nome = $_POST['nome'];
+    $mensagem = $_POST['mensagem'];
+
+    $sql = $pdo->prepare("INSERT INTO mensagens SET nome = :nome, msg = :msg, data_msg = NOW()");
+    $sql->bindValue(":nome", $nome);
+    $sql->bindValue(":msg", $mensagem);
+    $sql->execute();
+}
+
+?>
+
+<fieldset>
 <form method="post">
     Nome: <br/>
     <input type="text" name="nome" /><br/><br/>
@@ -8,6 +28,20 @@
 </form>
 </fieldset>
 <br/><br/>
-<strong>Nome da pessoa</strong><br/>
-A mensagem fica aqui
-<hr/>
+
+<?php
+$sql ="SELECT * FROM mensagens ORDER BY data_msg DESC";
+$sql = $pdo->query($sql);
+if($sql->rowCount() > 0){
+    foreach($sql->fetchAll() as $mensagem):
+    ?>
+        <strong><?php echo $mensagem['nome']; ?></strong><br/>
+        <?php echo $mensagem['msg']; ?>
+        <hr/>
+    <?php
+    endforeach;
+    } else {
+        echo "Não há mensagens.";
+    }
+?>
+
