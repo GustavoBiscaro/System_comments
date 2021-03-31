65c077325682f626965c60f39e7621ee19e737d6
Gustavo Miguel <gustavo.biscaro@gmail.com>
2021-03-31 15:26:56 -0300

    Create the basic HTML form

---
+++ index.php
@@ -0,0 +1,13 @@
+﻿<fieldset>
+<form method="post">
+    Nome: <br/>
+    <input type="text" name="nome" /><br/><br/>
+    Mensagem: <br/>
+    <textarea name="mensagem"></textarea> <br/><br/>
+    <input type="submit" value="Enviar Mensagem">
+</form>
+</fieldset>
+<br/><br/>
+<strong>Nome da pessoa</strong><br/>
+A mensagem fica aqui
+<hr/>
